2c0ec2be7ba878f36d931e039e51522f678f569e
Nathan <nathan.rendall@gmail.com>
2021-07-23 13:11:09 +0100

    can save list items using two inputs, name and artist.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,32 @@
 // 3. what code should i run when the event happens
 
 document.addEventListener('DOMContentLoaded', () => {
-   const albumForm = document.querySelector('#albumForm');
-   albumForm.addEventListener()
-
+   const albumForm = document.querySelector('#album-form');
+   albumForm.addEventListener('submit', handleSubmit);
 });
+
+const handleSubmit = event => {
+    event.preventDefault();
+    const albumItem = createAlbumItem(event.target);
+    const albumList = document.querySelector('#album-list');
+    albumList.appendChild(albumItem);
+};
+
+const createAlbumItem = form => {
+    const albumItem = document.createElement('li');
+    albumItem.classList.add('album-item');
+
+    const albumName = document.createElement('h3');
+    albumName.textContent = form.album.value;
+    albumItem.appendChild(albumName);
+
+    const albumArtist = document.createElement('h4');
+    albumArtist.textContent = form.artist.value;
+    albumItem.appendChild(albumArtist);
+
+    // const albumYear = document.createElement('h5');
+    // albumYear.textContent = form.year.value;
+    // albumItem.appendChild(albumYear);
+
+    return albumItem;
+};
