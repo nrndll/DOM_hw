418b5db494259c6c5676509c8d13ae85e0447367
Nathan <nathan.rendall@gmail.com>
2021-07-23 12:14:45 +0100

    inital commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// 1. Which element will dispatch the event
+// 2. which event should we listen for
+// 3. what code should i run when the event happens
+
+document.addEventListener('DOMContentLoaded', () => {
+   const albumForm = document.querySelector('#albumForm');
+   albumForm.addEventListener()
+
+});
