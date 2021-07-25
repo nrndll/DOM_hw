d80032fc01329b3a2a502dc79e9a8138ebf0d790
Nathan <nathan.rendall@gmail.com>
2021-07-25 17:16:43 +0100

    further positioning changes in CSS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-// 1. Which element will dispatch the event
-// 2. which event should we listen for
-// 3. what code should i run when the event happens
-
 document.addEventListener('DOMContentLoaded', () => {
    const albumForm = document.querySelector('#album-form');
    albumForm.addEventListener('submit', handleSubmit);
